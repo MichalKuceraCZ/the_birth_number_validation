79cc049304ab4de8d0905676d8711bb538344740
MichalKuceraCZ <114856544+MichalKuceraCZ@users.noreply.github.com>
2022-10-10 19:17:59 +0200

    vložení kódu

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import string_parsing
+# Napiste funkci validujici rodne cislo, funkce bere jako parameter string s rodnym cislem,
+# a vrati True nebo False v zavislosti na vysledku validace. K funkci pridejte positivni a negativni Pytest test case.
+# Zkuste použit programování s cyklem.
 
-cl_in: str = input('enter the birth number (you can use slash separator or not) --> ')
+def rodne_cislo(cislo):
+    number_in = []
+    for i in cislo:
+        if i.isdigit():
+            number_in += i
+        print(number_in)
+    return len(number_in) == 10
 
-slash_index = string_parsing.slash_find(cl_in)
 
-if slash_index == -1:
-    print('has been entered birth number without slash')
-else:
-    print(f"Slash is on index:  {slash_index} in the string.")
+def rodne_cislo1(cislo):
+    while cislo:
+        if cislo == "/":
+            break
+        cislo += cislo
