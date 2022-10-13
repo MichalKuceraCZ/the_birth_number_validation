71a2aaa1d7a21f74c8be7674dae4f5ae9f275112
MichalKuceraCZ <114856544+MichalKuceraCZ@users.noreply.github.com>
2022-10-13 17:26:52 +0200

    finální verze

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Napiste funkci validujici rodne cislo, funkce bere jako parameter string s rodnym cislem,
 # a vrati True nebo False v zavislosti na vysledku validace. K funkci pridejte positivni a negativni Pytest test case.
 # Zkuste použit programování s cyklem.
+#
+import re
+
 
 def rodne_cislo(cislo):
     number_in = []
@@ -10,8 +13,18 @@ def rodne_cislo(cislo):
         print(number_in)
     return len(number_in) == 10
 
+
+# def rodne_cislo1(cislo):
+#     if cislo == len(cislo) == 10:
+#         return True
+#     else:
+#         return False
+
+
 def rodne_cislo1(cislo):
-    while cislo:
-        if cislo == "/":
-            break
-        return len(cislo) == 11
+    cislo = re.sub(r"/", "", cislo)         # odebere / v rodném čísle
+    print(cislo)
+    if len(cislo) == 10:
+        return True
+    else:
+        return False
